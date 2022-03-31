b38b68d45edf8ac966b548e12ce318bdd9606566
caio-magnani <49353875+caio-magnani@users.noreply.github.com>
2022-03-31 02:24:51 -0300

    Update LandingPageController.java

--- Implementacao/aluguel-de-automoveis/src/main/java/com/lab2/alugueldeautomoveis/controller/LandingPageController.java
+++ Implementacao/aluguel-de-automoveis/src/main/java/com/lab2/alugueldeautomoveis/controller/LandingPageController.java
@@ -1,6 +1,7 @@
 package com.lab2.alugueldeautomoveis.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
