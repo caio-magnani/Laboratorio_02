ef42917326010ff473fae6ceb810a5bcf973e0bc
caio-magnani <49353875+caio-magnani@users.noreply.github.com>
2022-03-31 02:25:04 -0300

    Create InvalidLoginException.java

---
+++ Implementacao/aluguel-de-automoveis/src/main/java/com/lab2/alugueldeautomoveis/controller/util/exception/InvalidLoginException.java
@@ -0,0 +1,7 @@
+package com.lab2.alugueldeautomoveis.controller.util.exception;
+
+public class InvalidLoginException extends Exception{
+    public InvalidLoginException(String msg){
+        super(msg);
+    }
+}
